7ae0ae4468e252e6f427ddc8e6cc609549531aae
Nazari Gonzalez <nazari.nz@gmail.com>
2024-09-29 00:01:46 +0100

    new example

---
+++ examples/draw_image_crop.rs
@@ -0,0 +1,44 @@
+use rkit::draw::{create_draw_2d, Sprite};
+use rkit::gfx::{self, Color};
+use rkit::math::{vec2, Vec2};
+
+struct State {
+    img: Sprite,
+}
+
+impl State {
+    fn new() -> Result<Self, String> {
+        let sprite = draw::create_sprite()
+            .from_image(include_bytes!("assets/rust-logo-512x512.png"))
+            .build()?;
+
+        Ok(Self { img: sprite })
+    }
+}
+
+fn main() -> Result<(), String> {
+    rkit::init_with(|| State::new().unwrap())
+        .on_update(update)
+        .run()
+}
+
+fn update(state: &mut State) {
+    let Vec2 { x: ww, y: hh } = state.img.size();
+
+    let mut draw = create_draw_2d();
+    draw.clear(Color::WHITE);
+
+    // Right side of the logo
+    draw.image(&state.img)
+        .position(vec2(100.0, 50.0))
+        .size(vec2(ww * 0.5, hh))
+        .crop(vec2(ww * 0.5, 0.0), vec2(ww * 0.5, hh));
+
+    // Left side of the logo
+    draw.image(&state.img)
+        .position(vec2(450.0, 50.0))
+        .size(vec2(ww * 0.5, hh))
+        .crop(vec2(0.0, 0.0), vec2(ww * 0.5, hh));
+
+    gfx::render_to_frame(&draw).unwrap();
+}
