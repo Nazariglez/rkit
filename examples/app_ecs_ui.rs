1c0c9274df32182e0a975eba8442cebee440805c
Nazari Gonzalez <nazari.nz@gmail.com>
2025-02-06 20:10:56 +0000

    wip ecs taffy?

--- examples/app_ecs_ui.rs
+++ examples/app_ecs_ui.rs
@@ -1,177 +1,298 @@
-use draw::{create_draw_2d, Draw2D, Transform2DBuilder};
-use rkit::gfx::Color;
-use rkit::math::{vec2, Vec2};
+use draw::create_draw_2d;
+use rkit::gfx::{self, Color};
+use rkit::math::Vec2;
 use rkit::prelude::*;
-use rkit::{gfx, time};
-use std::fmt::Alignment;
-use std::ops::Rem;
+use rustc_hash::FxHashMap;
 
-use corelib::math::Vec3;
 use taffy::prelude::*;
-
-#[derive(Resource, Default)]
-pub struct TTree {
-    tree: TaffyTree<()>,
-}
 
 fn main() -> Result<(), String> {
     App::new()
         .add_plugin(AddMainPlugins::default())
-        .add_resource(TTree::default())
+        .add_plugin(UIPlugin)
         .add_systems(OnSetup, setup_system)
         .add_systems(OnRender, draw_system)
         .run()
 }
 
-fn setup_system(mut tree: ResMut<TTree>) {
-    println!("whatever");
+pub struct UIPlugin;
+impl Plugin for UIPlugin {
+    fn apply(self, app: App) -> App {
+        app.add_resource(UILayouts::default())
+            .add_systems(OnPostUpdate, compute_layout_system)
+    }
 }
 
-fn draw_system(mut tree: ResMut<TTree>, win: Res<Window>) {
-    let size = win.size();
-    let tree = &mut tree.tree;
-    let header_node = tree
-        .new_leaf(Style {
-            size: Size {
-                width: length(size.x),
-                height: length(100.0),
-            },
-            ..Default::default()
-        })
-        .unwrap();
+fn compute_layout_system(mut query: Query<&mut UINode>, mut layouts: ResMut<UILayouts>) {
+    layouts.layouts.iter_mut().for_each(|(id, layout)| {
+        let updated = layout.update();
+        // TODO: update UINode info we need to use UIStyle as input and UIWhatever as output
+        layout.tree.child_ids(parent_node_id)
+    });
+}
 
-    let body_node = tree
-        .new_leaf(Style {
-            size: Size {
-                width: length(size.x),
-                height: auto(),
-            },
-            flex_grow: 1.0,
-            display: Display::Flex,
-            align_items: Some(AlignItems::Center),
-            justify_content: Some(JustifyContent::Center),
-            ..Default::default()
-        })
-        .unwrap();
+#[derive(Component)]
+pub struct UINode {
+    pub id: NodeId,
+    pub layout: UILayoutId,
+    pub position: Vec2,
+    pub size: Vec2,
+    // TODO: extras as content_size, etc...
+}
 
-    let footer_node = tree
-        .new_leaf(Style {
-            size: Size {
-                width: length(size.x),
-                height: length(100.0),
-            }, // Fixed height
-            ..Default::default()
-        })
-        .unwrap();
+#[derive(Debug)]
+struct LayoutData {
+    id: UILayoutId,
+    dirty: bool,
+    tree: TaffyTree<Entity>,
+    size: Vec2,
+    root: NodeId,
+}
 
-    let root_node = tree
-        .new_with_children(
-            Style {
-                flex_direction: FlexDirection::Column,
-                size: Size {
-                    width: length(size.x),
-                    height: length(size.y),
-                },
-                ..Default::default()
-            },
-            &[header_node, body_node, footer_node],
-        )
-        .unwrap();
-
-    // new
-    let content = tree
-        .new_leaf(Style {
-            size: Size {
-                width: percent(0.9),
-                height: percent(0.9),
-            },
-            display: Display::Flex,
-            flex_direction: FlexDirection::Row,
-            justify_content: Some(JustifyContent::SpaceBetween),
-            gap: Size {
-                width: length(20.0),
-                height: length(0.0),
-            },
-            padding: Rect::length(30.0),
-            ..Default::default()
-        })
-        .unwrap();
-    tree.set_children(body_node, &[content]);
-
-    for _ in (0..5) {
-        let col = tree
-            .new_leaf(Style {
-                flex_grow: 1.0,
-                size: Size {
-                    width: auto(),
-                    height: percent(1.0),
-                },
-                ..Default::default()
-            })
-            .unwrap();
-
-        tree.add_child(content, col);
+impl LayoutData {
+    fn new(id: UILayoutId, size: Vec2) -> Self {
+        let mut tree = TaffyTree::<Entity>::new();
+        let root = tree.new_leaf(Style::default()).unwrap();
+        Self {
+            id,
+            dirty: true,
+            tree,
+            root,
+            size,
+        }
     }
 
-    tree.compute_layout(root_node, Size::MAX_CONTENT).unwrap();
+    pub fn set_size(&mut self, size: Vec2) {
+        self.size = size;
+        self.dirty = true;
+    }
 
+    pub fn add_node(&mut self, entity: Entity, style: Style, parent: Option<NodeId>) -> NodeId {
+        self.dirty = true;
+        let node_id = self.tree.new_leaf_with_context(style, entity).unwrap();
+        if let Some(parent) = parent {
+            self.tree.add_child(parent, node_id).unwrap();
+        }
+        node_id
+    }
+
+    pub fn update(&mut self) -> bool {
+        if !self.dirty {
+            return false;
+        }
+
+        self.dirty = false;
+        self.tree
+            .compute_layout(
+                self.root,
+                Size {
+                    width: AvailableSpace::Definite(self.size.x),
+                    height: AvailableSpace::Definite(self.size.y),
+                },
+            )
+            .unwrap();
+
+        return true;
+    }
+}
+
+#[derive(Default, Debug, Resource)]
+pub struct UILayouts {
+    layouts: FxHashMap<UILayoutId, LayoutData>,
+}
+
+impl UILayouts {
+    fn add(
+        &mut self,
+        entity: Entity,
+        layout_id: UILayoutId,
+        style: Style,
+        parent: Option<NodeId>,
+    ) -> NodeId {
+        let layout = self
+            .layouts
+            .entry(layout_id)
+            .or_insert_with(|| LayoutData::new(layout_id, Vec2::splat(10.0)));
+
+        layout.add_node(entity, style, parent)
+    }
+}
+
+pub struct SpawnUICommandBuilder<'c, 'w, 's> {
+    cmds: Option<&'c mut Commands<'w, 's>>,
+    count: usize,
+    stack: Vec<usize>,
+    layout: UILayoutId,
+    bundles: Option<Vec<Box<dyn FnOnce(&mut World, &mut FxHashMap<usize, NodeId>) + Send>>>,
+}
+
+pub struct SpawnUICommand {
+    bundles: Vec<Box<dyn FnOnce(&mut World, &mut FxHashMap<usize, NodeId>) + Send>>,
+}
+
+#[derive(Component, Default)]
+pub struct UIStyle {}
+
+impl UIStyle {
+    fn to_taffy(&self) -> Style {
+        Style::default()
+    }
+}
+
+impl SpawnUICommandBuilder<'_, '_, '_> {
+    pub fn add<T: Bundle>(&mut self, bundle: T) -> &mut Self {
+        self.count += 1;
+        let id = self.count;
+        let parent_id = self.stack.last().cloned();
+        let layout_id = self.layout;
+        self.bundles.as_mut().unwrap().push(Box::new(
+            move |world: &mut World, ids: &mut FxHashMap<usize, NodeId>| {
+                let (entity, style) = world
+                    .spawn(UIStyle::default())
+                    .insert(bundle)
+                    .get_components::<(Entity, &UIStyle)>()
+                    .map(|(e, style)| (e, style.to_taffy()))
+                    .unwrap();
+
+                let mut layouts = world.get_resource_mut::<UILayouts>().unwrap();
+                let parent_id = parent_id.and_then(|p_id| ids.get(&p_id)).cloned();
+                let node_id = layouts.add(entity, layout_id, style, parent_id);
+                ids.insert(id, node_id);
+
+                world.entity_mut(entity).insert(UINode {
+                    layout: layout_id,
+                    id: node_id,
+                });
+            },
+        ));
+        self.stack.push(self.count);
+        self
+    }
+
+    pub fn with_children<F: FnOnce(&mut Self)>(&mut self, cb: F) -> &mut Self {
+        cb(self);
+        self.stack.pop();
+        self
+    }
+}
+
+impl Drop for SpawnUICommandBuilder<'_, '_, '_> {
+    fn drop(&mut self) {
+        let bundles = self.bundles.take();
+        let command = SpawnUICommand {
+            bundles: bundles.unwrap(),
+        };
+        let cmds = self.cmds.take().unwrap();
+        cmds.queue(command);
+    }
+}
+
+pub trait CommandSpawnUIExt<'w, 's> {
+    fn spawn_ui<'c, T: Bundle>(
+        &'c mut self,
+        layout: UILayoutId,
+        bundle: T,
+    ) -> SpawnUICommandBuilder<'c, 'w, 's>;
+}
+
+impl<'w, 's> CommandSpawnUIExt<'w, 's> for Commands<'w, 's> {
+    fn spawn_ui<'c, T: Bundle>(
+        &'c mut self,
+        layout: UILayoutId,
+        bundle: T,
+    ) -> SpawnUICommandBuilder<'c, 'w, 's> {
+        let mut builder = SpawnUICommandBuilder {
+            cmds: Some(self),
+            count: 0,
+            stack: vec![],
+            bundles: Some(vec![]),
+            layout,
+        };
+
+        builder.add(bundle);
+        builder
+    }
+}
+
+impl Command for SpawnUICommand {
+    fn apply(self, world: &mut World) {
+        let Self { bundles } = self;
+
+        let mut table = FxHashMap::default();
+        for cb in bundles {
+            cb(world, &mut table);
+        }
+    }
+}
+
+#[derive(Component, Deref)]
+pub struct UIPos(Vec2);
+
+#[derive(Component, Deref)]
+pub struct UITint(Color);
+
+#[derive(Component, Deref)]
+pub struct UISize(Vec2);
+
+fn setup_system(mut cmds: Commands, win: Res<Window>) {
+    cmds.spawn_ui(
+        UILayoutId("main"),
+        (UIPos(Vec2::ZERO), UITint(Color::WHITE), UISize(win.size())),
+    )
+    .with_children(|cmd| {
+        cmd.add(((
+            UIPos(Vec2::splat(20.0)),
+            UITint(Color::ORANGE),
+            UISize(Vec2::splat(400.0)),
+        ),))
+            .with_children(|cmd| {
+                cmd.add((
+                    UIPos(Vec2::splat(20.0)),
+                    UITint(Color::GREEN),
+                    UISize(Vec2::splat(40.0)),
+                ));
+            });
+
+        cmd.add((
+            UIPos(Vec2::splat(500.0)),
+            UITint(Color::BLUE),
+            UISize(Vec2::splat(40.0)),
+        ));
+    });
+
+    cmds.spawn_ui(
+        UILayoutId("main"),
+        (
+            UIPos(Vec2::splat(20.0) + Vec2::splat(25.0)),
+            UITint(Color::ORANGE),
+            UISize(Vec2::splat(400.0)),
+        ),
+    )
+    .with_children(|cmd| {
+        cmd.add((
+            UIPos(Vec2::splat(20.0) + Vec2::splat(25.0)),
+            UITint(Color::GREEN),
+            UISize(Vec2::splat(40.0)),
+        ));
+
+        cmd.add((
+            UIPos(Vec2::splat(500.0) + Vec2::splat(25.0)),
+            UITint(Color::BLUE),
+            UISize(Vec2::splat(40.0)),
+        ));
+    });
+}
+
+fn draw_system(query: Query<(&UIPos, &UITint, &UISize)>) {
     let mut draw = create_draw_2d();
     draw.clear(Color::BLACK);
 
-    const COLORS: [Color; 19] = [
-        Color::WHITE,
-        Color::BLACK,
-        Color::RED,
-        Color::GREEN,
-        Color::BLUE,
-        Color::YELLOW,
-        Color::MAGENTA,
-        Color::SILVER,
-        Color::GRAY,
-        Color::OLIVE,
-        Color::PURPLE,
-        Color::MAROON,
-        Color::BROWN,
-        Color::SADDLE_BROWN,
-        Color::AQUA,
-        Color::TEAL,
-        Color::NAVY,
-        Color::ORANGE,
-        Color::PINK,
-    ];
-
-    fn draw_node(
-        node_id: NodeId,
-        tree: &mut TaffyTree<()>,
-        draw: &mut Draw2D,
-        mut color_idx: usize,
-    ) {
-        let c = COLORS[color_idx.rem(COLORS.len())];
-        println!("c {c:?} idx {color_idx}");
-
-        let layout = tree.layout(node_id).unwrap();
-        println!("\n{node_id:?}:\n{layout:?}");
-        draw.push_matrix(
-            Transform2DBuilder::default()
-                .set_translation(vec2(layout.location.x, layout.location.y))
-                .build()
-                .as_mat3(),
-        );
-
-        draw.rect(Vec2::ZERO, vec2(layout.size.width, layout.size.height))
-            .color(c);
-
-        for child_id in tree.children(node_id).unwrap() {
-            color_idx += 1;
-            draw_node(child_id, tree, draw, color_idx);
-        }
-
-        draw.pop_matrix();
+    for (pos, tint, size) in &query {
+        draw.rect(Vec2::ZERO, size.0).translate(pos.0).color(tint.0);
     }
-
-    println!("--------------------");
-    draw_node(root_node, tree, &mut draw, 0);
-    println!("++++++++++++++++++++");
 
     gfx::render_to_frame(&draw).unwrap();
 }
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+pub struct UILayoutId(&'static str);
